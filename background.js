5039d3024ad5c76d4a70542a9449ad06f6281b5a
shiku <hoge@piyo.com>
2019-01-20 22:56:35 +0900

    backup for linebreak

--- background.js
+++ background.js
@@ -14,9 +14,15 @@ chrome.contextMenus.onClicked.addListener(function(info, tab){
     chrome.storage.local.get(function(obj) {
 	var summary_number = 3;
 	var minimum_length = 5;
-
+	var separator = [".","。"];
+	var lang = "en";
 	summary_number = obj.summary_number;
 	minimum_length = obj.minimum_length;
+	// group of separator is given by text split by " ".
+	// it is very bad hack...
+	separator = obj.separator.split(" ");
+	alert(separator);
+	lang = obj.lang;
 	// not so good solution ....
 	if(info.selectionText.length < 20){
 	    if(tab.id>=0){
@@ -30,11 +36,16 @@ chrome.contextMenus.onClicked.addListener(function(info, tab){
 	//getinfo();
 	
 	// summarize and push
-	summary = preproc_en(info.selectionText, summary_number, minimum_length);
+	if (lang == "ja"){
+	    summary = preproc_ja(info.selectionText, summary_number, minimum_length, separator);
+	}else{
+	    summary = preproc_en(info.selectionText, summary_number, minimum_length, separator);
+	}
 	summary_alert = "";
 	for(var i=0; i<summary.length; ++i){
 	    summary_alert += "-> " + summary[i] +".\n\n";
 	}
+
 	alert(summary_alert);
 	if(tab.id>=0){
 	    chrome.tabs.sendMessage(tab.id, {"command":"fill", "summary":summary});
--- popup.js
+++ popup.js
@@ -6,6 +6,13 @@ $(function () {
 	if ("minimum_length" in obj) {
 	    $("input[name='minimum_length']").val(obj.minimum_length);
 	}
+	if ("lang" in obj) {
+	    $("select[name='lang']").val(obj.lang);
+	}
+	if ("separator" in obj) {
+	    $("select[name='separator']").val(obj.separator);
+	}
+
 	$("#register").on("click", function() {
 	    update_settings();
 	});
@@ -17,6 +24,8 @@ function update_settings() {
     let obj = {};
     obj.summary_number = $("input[name='summary_number']").val();
     obj.minimum_length = $("input[name='minimum_length']").val();
+    obj.lang = $("select[name='lang']").val();
+    obj.separator = $("select[name='separator']").val();
     chrome.storage.local.set(obj, function(){
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	    chrome.tabs.sendMessage(tabs[0].id, {type: "update"}, null);
